a065957e5a4d8079cb41b381512aab7b4f07c626
a b <anders9943@hotmail.com>
2016-03-23 15:01:43 +0100

    Managed to get the CLR stored procedure working

--- oneRealTrueTireBiz/oneRealTrueTireBiz/sp_numberOfBookingForDay.cs
+++ oneRealTrueTireBiz/oneRealTrueTireBiz/sp_numberOfBookingForDay.cs
@@ -7,29 +7,17 @@ using Microsoft.SqlServer.Server;
 public partial class StoredProcedures
 {
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static void StoredProcedureOne(SqlDateTime DT)
+    public static void StoredProcedureOne(DateTime DT)
     {
         using (SqlConnection conn = new SqlConnection("context connection=true"))
         {
             SqlCommand CheckSpecificDate = new SqlCommand();
             SqlParameter selectDayParam = new SqlParameter("@SelectedDay", SqlDbType.Date);
             selectDayParam.Value = DT;
-
-
             CheckSpecificDate.Parameters.Add(selectDayParam);
+            CheckSpecificDate.CommandText = "SELECT dbo.TimeSlots.SlotTime, dbo.TireChange.TimeID FROM dbo.TimeSlots INNER JOIN dbo.TireChange ON dbo.TimeSlots.TimeID = dbo.TireChange.TimeID WHERE dbo.TimeSlots.SlotTime < @SelectedDay ORDER BY SlotTime ASC";
+            SqlContext.Pipe.ExecuteAndSend(CheckSpecificDate);
 
-
-            CheckSpecificDate.CommandText = "SELECT dbo.TimeSlots.SlotTime, dbo.TireChange.TimeID" +
-                                                "FROM dbo.TimeSlots INNER JOIN" +
-                                                "dbo.TireChange ON dbo.TimeSlots.TimeID = dbo.TireChange.TimeID" +
-                                                "WHERE dbo.TimeSlots.SlotTime < @SelectedDay" +
-                                                "ORDER BY SlotTime ASC";
-
-            CheckSpecificDate.Connection = conn;
-
-            conn.Open();
-            CheckSpecificDate.ExecuteNonQuery();
-            conn.Close();
         }
 
     }
